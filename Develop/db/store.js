20eee1850b30ba1dc753188616d2056192565b04
John Fan <jf3287@columbia.edu>
2020-11-04 21:54:09 -0500

    added store class

---
+++ Develop/db/store.js
@@ -0,0 +1,32 @@
+const util = require("util");
+const fs = require("fs");
+
+// This package will be used to generate our unique ids. https://www.npmjs.com/package/uuid
+const uuidv1 = require("uuid/v1");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+  read() {
+    return readFileAsync("db/db.json", "utf8");
+  }
+
+  write(note) {
+    return writeFileAsync("db/db.json", JSON.stringify(note));
+  }
+
+  getNotes() {
+    return this.read().then((notes) => {
+      let parsedNotes;
+
+      // If notes isn't an array or can't be turned into one, send back a new empty array
+      try {
+        parsedNotes = [].concat(JSON.parse(notes));
+      } catch (err) {
+        parsedNotes = [];
+      }
+
+      return parsedNotes;
+    });
+  }
